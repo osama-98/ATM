b1457642943b08649d75b733e583cb92076b9945
osama-98 <osama.sada98@gmail.com>
2022-12-02 21:23:57 +0300

    home

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,40 +6,55 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionType;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Transaction
- * 
+ *
  * @property int $id
  * @property float $value
  * @property string $type
  * @property int $user_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * 
+ *
  * @property User $user
+ *
+ * @method static Builder deposit()
+ * @method static Builder withdraw()
  *
  * @package App\Models
  */
 class Transaction extends Model
 {
-	protected $table = 'transactions';
+    protected $table = 'transactions';
 
-	protected $casts = [
-		'value' => 'float',
-		'user_id' => 'int'
-	];
+    protected $casts = [
+        'value' => 'float',
+        'user_id' => 'int'
+    ];
 
-	protected $fillable = [
-		'value',
-		'type',
-		'user_id'
-	];
+    protected $fillable = [
+        'value',
+        'type',
+        'user_id'
+    ];
 
-	public function user()
-	{
-		return $this->belongsTo(User::class);
-	}
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeDeposit($query)
+    {
+        return $query->where('transactions.type', TransactionType::Deposit);
+    }
+
+    public function scopeWithdraw($query)
+    {
+        return $query->where('transactions.type', TransactionType::Withdraw);
+    }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -43,7 +43,9 @@
                     <div class="card-title d-flex flex-column">
                         <!--begin::Info-->
                         <div class="d-flex align-items-center">
-                            <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">69,700</span>
+                            <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">
+                                {{ \App\Models\Transaction::count() }}
+                            </span>
                         </div>
                         <!--end::Info-->
                         <!--begin::Subtitle-->
@@ -66,7 +68,9 @@
                             <div class="text-gray-500 flex-grow-1 me-4">Deposit Transactions</div>
                             <!--end::Label-->
                             <!--begin::Stats-->
-                            <div class="fw-bolder text-gray-700 text-xxl-end">7,660</div>
+                            <div class="fw-bolder text-gray-700 text-xxl-end">
+                                {{ \App\Models\Transaction::deposit()->count() }}
+                            </div>
                             <!--end::Stats-->
                         </div>
                         <!--end::Label-->
@@ -79,7 +83,9 @@
                             <div class="text-gray-500 flex-grow-1 me-4">Withdraw Transactions</div>
                             <!--end::Label-->
                             <!--begin::Stats-->
-                            <div class="fw-bolder text-gray-700 text-xxl-end">2,820</div>
+                            <div class="fw-bolder text-gray-700 text-xxl-end">
+                                {{ \App\Models\Transaction::withdraw()->count() }}
+                            </div>
                             <!--end::Stats-->
                         </div>
                     </div>
@@ -88,46 +94,25 @@
                 <!--end::Card body-->
             </div>
         </div>
-        <!--end::Col-->
-        <!--begin::Col-->
         <div class="col-xxl-6">
-            <!--begin::Engage widget 10-->
             <div class="card card-flush h-100">
                 <!--begin::Body-->
                 <div class="card-body d-flex flex-column justify-content-between mt-9 bgi-no-repeat bgi-size-cover bgi-position-x-center pb-0"
                      style="background-position: 100% 50%; background-image:url('/metronic8/demo1/assets/media/stock/900x600/42.png')">
-                    <!--begin::Wrapper-->
                     <div class="mb-10">
-                        <!--begin::Title-->
                         <div class="fs-2hx fw-bold text-gray-800 text-center mb-13">
                             <span class="me-2">Try our all new Enviroment with
                                 <br />
-                                <label class="position-relative d-inline-block text-danger">
-															<a href="/" class="text-danger opacity-75-hover">Pro Plan</a>
-                                    <!--begin::Separator-->
-															<span class="position-absolute opacity-15 bottom-0 start-0 border-4 border-danger border-bottom w-100"></span>
-                                    <!--end::Separator-->
-														</label>
+                                <span class="position-relative d-inline-block text-danger">
+                                    <a href="/" class="text-danger opacity-75-hover">Pro Plan</a>
+                                    <span class="position-absolute opacity-15 bottom-0 start-0 border-4 border-danger border-bottom w-100"></span>
+                                </span>
                             </span>
                             for Free
                         </div>
-                        <!--end::Title-->
-                        <!--begin::Action-->
-                        <div class="text-center">
-                            <a href='#' class="btn btn-sm btn-dark fw-bold" data-bs-toggle="modal" data-bs-target="#kt_modal_upgrade_plan">Upgrade Now</a>
-                        </div>
-                        <!--begin::Action-->
                     </div>
-                    <!--begin::Wrapper-->
-                    <!--begin::Illustration-->
-                    <img class="mx-auto h-150px h-lg-200px theme-light-show" src="/metronic8/demo1/assets/media/illustrations/misc/upgrade.svg" alt="" />
-                    <img class="mx-auto h-150px h-lg-200px theme-dark-show" src="/metronic8/demo1/assets/media/illustrations/misc/upgrade-dark.svg" alt="" />
-                    <!--end::Illustration-->
                 </div>
-                <!--end::Body-->
             </div>
-            <!--end::Engage widget 10-->
         </div>
-        <!--end::Col-->
     </div>
 @endsection
