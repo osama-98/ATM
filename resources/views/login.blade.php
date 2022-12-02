c5f9d89f5ab3419d41ae1bd8bcb254dcd1eaaf20
osama-98 <osama.sada98@gmail.com>
2022-12-02 18:03:29 +0300

    auth

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.auth')
 
+@section('title', 'login')
+
 @section('content')
     <div class="form">
         <form class="login-form">
             <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="password" placeholder="password" autocomplete="off" />
             <button>login</button>
             <p class="message">Not registered? <a href="{{ route('register') }}">Create an account</a></p>
         </form>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.auth')
 
+@section('title', 'register')
+
 @section('content')
     <div class="form">
         <form class="register-form">
             <input type="text" placeholder="name" name="name" id="name" />
             <input type="text" placeholder="username" name="username" id="username" />
-            <input type="password" placeholder="password" name="password" id="password" />
-            <input type="password" placeholder="confirm password" name="current_password" id="current_password" />
+            <input type="password" autocomplete="off" placeholder="password" name="password" id="password" />
+            <input type="password" autocomplete="off" placeholder="confirm password" name="current_password" id="current_password" />
             <button>create</button>
             <p class="message">Already registered? <a href="{{ route('login') }}">Sign In</a></p>
         </form>
