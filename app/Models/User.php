514857af7420041e8a15596e64169294d23f74c3
osama-98 <osama.sada98@gmail.com>
2022-12-02 16:55:57 +0300

    database

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,6 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
