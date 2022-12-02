6a9ee2c62edb4ee2250c168ddfd7d56cc2c07e04
osama-98 <osama.sada98@gmail.com>
2022-12-03 01:59:51 +0300

    user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,12 +17,14 @@
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">Register</a>
+                            </li>
+                        @endguest
 
                         {{-- <li class="nav-item">
                     <a class="nav-link" href="#featured">Featured</a>
