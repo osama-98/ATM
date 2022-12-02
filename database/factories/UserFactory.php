7783d475adc40b678d585988684a5658d37884f6
osama-98 <osama.sada98@gmail.com>
2022-12-03 01:58:41 +0300

    user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Enums\UserRole;
+use App\Enums\UserStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -19,6 +20,7 @@ class UserFactory extends Factory
             'name' => 'Admin',
             'username' => 'osama_admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'status' => UserStatus::Active,
             'role' => UserRole::Admin
         ];
     }
