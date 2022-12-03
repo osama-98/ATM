f6b3400a388a02cee69971923a24dc546bf7e551
osama-98 <osama.sada98@gmail.com>
2022-12-03 17:37:09 +0300

    factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,8 +18,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => 'Admin',
-            'username' => 'osama_admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'username' => 'student',
+            'password' => bcrypt('student'),
             'status' => UserStatus::Active,
             'role' => UserRole::Admin
         ];
